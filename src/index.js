f514a323b62f982f6e626c4d7cca838587fd5498
deltapoint <60572499+deltapoint@users.noreply.github.com>
2020-03-22 23:24:06 +0300

    feat: implement 'brackets' task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let splitstr = str.split('');
+
+  for (let i = 0; i < splitstr.length; i++) {
+    for(let j = 0; j < bracketsConfig.length; j++){
+      let current = splitstr[i];
+      if(current == bracketsConfig[j][0]) {
+        let last = stack[stack.length-1];
+        if(current == bracketsConfig[j][1] && last == bracketsConfig[j][0]){
+          stack.pop();
+        } else {
+          stack.push(current);
+        } break;
+      }
+      if(current == bracketsConfig[j][1]){
+        let last = stack[stack.length-1];
+        if(current == bracketsConfig[j][1] && last == bracketsConfig[j][0]){
+          stack.pop();
+        } else return false;
+      } 
+    }
+  }
+  return stack.length == 0; 
 }
